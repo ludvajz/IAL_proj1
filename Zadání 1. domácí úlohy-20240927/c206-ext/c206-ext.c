941ea96d30da0476563c7ad651a5f95c47d48987
ludvajz <145606890+ludvajz@users.noreply.github.com>
2024-09-30 22:39:58 +0200

    Update c206-ext.c
    working c206 ext

--- Zadání 1. domácí úlohy-20240927/c206-ext/c206-ext.c
+++ Zadání 1. domácí úlohy-20240927/c206-ext/c206-ext.c
@@ -3,6 +3,13 @@
  *  Rozšíření pro příklad c206.c (Dvousměrně vázaný lineární seznam)
  *  Vytvořil: Daniel Dolejška, září 2024
  */
+/**
+ * @file c206-ext.c
+ * @author xludvir00
+ * @brief Rozšíření pro příklad c206.c (Dvousměrně vázaný lineární seznam)
+ * @date 2024-09-30
+ * 
+*/
 
 #include "c206-ext.h"
 
@@ -27,9 +34,72 @@ bool solved;
  * @param packetLists Ukazatel na inicializovanou strukturu dvousměrně vázaného seznamu
  * @param packet Ukazatel na strukturu přijatého paketu
  */
-void receive_packet( DLList *packetLists, PacketPtr packet ) {
-	solved = false; /* V případě řešení, smažte tento řádek! */
+void receive_packet(DLList *packetLists, PacketPtr packet) {
+    QosPacketListPtr qosPacketList = NULL;
+
+    // Prohledáme seznam, jestli už existuje fronta pro danou prioritu
+    DLL_First(packetLists);
+    while (DLL_IsActive(packetLists)) {
+        QosPacketListPtr currentList;
+        DLL_GetValue(packetLists, (long *)&currentList);
+        if (currentList->priority == packet->priority) {
+            qosPacketList = currentList;
+            break;
+        }
+        DLL_Next(packetLists);
+    }
+
+    // Pokud fronta pro danou prioritu neexistuje, vytvoříme ji
+    if (qosPacketList == NULL) {
+        qosPacketList = (QosPacketListPtr)malloc(sizeof(QosPacketList));
+        if (qosPacketList == NULL) {
+            //DLL_Error();
+            return;
+        }
+        qosPacketList->priority = packet->priority;
+        qosPacketList->list = (DLList *)malloc(sizeof(DLList));
+        if (qosPacketList->list == NULL) {
+            free(qosPacketList);
+            //DLL_Error();
+            return;
+        }
+        DLL_Init(qosPacketList->list);
+        DLL_InsertLast(packetLists, (long)qosPacketList);
+    }
+
+    // Přidáme nový paket do fronty (na konec)
+    DLL_InsertLast(qosPacketList->list, (long)packet);
+
+    // Kontrola, zda fronta přesáhla MAX_PACKET_COUNT
+    if (qosPacketList->list->currentLength > MAX_PACKET_COUNT) {
+        int index = 0;
+        DLL_First(qosPacketList->list);
+        DLLElementPtr elementToRemove = NULL;
+
+        // Procházíme seznam a mažeme každý druhý prvek
+        while (DLL_IsActive(qosPacketList->list)) {
+            if (index % 2 == 1) {
+                elementToRemove = qosPacketList->list->activeElement;
+                DLL_Next(qosPacketList->list); // Přesuneme aktivní prvek na další
+                if (elementToRemove == qosPacketList->list->firstElement) {
+                    DLL_DeleteFirst(qosPacketList->list); // Smažeme první prvek
+                } else if (elementToRemove == qosPacketList->list->lastElement) {
+                    DLL_DeleteLast(qosPacketList->list);  // Smažeme poslední prvek
+                } else {
+                    // Smažeme prvek uvnitř seznamu
+                    elementToRemove->previousElement->nextElement = elementToRemove->nextElement;
+                    elementToRemove->nextElement->previousElement = elementToRemove->previousElement;
+                    free(elementToRemove);
+                    qosPacketList->list->currentLength--;
+                }
+            } else {
+                DLL_Next(qosPacketList->list); // Posuneme se na další prvek
+            }
+            index++;
+        }
+    }
 }
+
 
 /**
  * Tato metoda simuluje výběr síťových paketů k odeslání. Výběr respektuje
@@ -46,6 +116,45 @@ void receive_packet( DLList *packetLists, PacketPtr packet ) {
  * @param outputPacketList Ukazatel na seznam paketů k odeslání
  * @param maxPacketCount Maximální počet paketů k odeslání
  */
-void send_packets( DLList *packetLists, DLList *outputPacketList, int maxPacketCount ) {
-	solved = false; /* V případě řešení, smažte tento řádek! */
+void send_packets(DLList *packetLists, DLList *outputPacketList, int maxPacketCount) {
+    int sentPackets = 0;
+
+    // Procházíme fronty podle priority (od nejvyšší k nejnižší)
+    while (sentPackets < maxPacketCount) {
+        QosPacketListPtr highestPriorityList = NULL;
+        DLL_First(packetLists);
+
+        // Najdeme frontu s nejvyšší prioritou, která obsahuje pakety
+        while (DLL_IsActive(packetLists)) {
+            QosPacketListPtr currentList;
+            DLL_GetValue(packetLists, (long *)&currentList);
+
+            if (highestPriorityList == NULL || currentList->priority > highestPriorityList->priority) {
+                if (currentList->list->firstElement != NULL) {
+                    highestPriorityList = currentList;
+                }
+            }
+
+            DLL_Next(packetLists);
+        }
+
+        // Pokud nejsou žádné fronty s pakety, ukončíme odesílání
+        if (highestPriorityList == NULL) {
+            break;
+        }
+
+        // Odesíláme pakety z fronty s nejvyšší prioritou
+        while (sentPackets < maxPacketCount && highestPriorityList->list->firstElement != NULL) {
+            long packet;
+            DLL_GetFirst(highestPriorityList->list, &packet);
+
+            // Přidáme paket do výstupního seznamu
+            DLL_InsertLast(outputPacketList, packet);
+
+            // Odebereme paket z fronty
+            DLL_DeleteFirst(highestPriorityList->list);
+
+            sentPackets++;
+        }
+    }
 }
